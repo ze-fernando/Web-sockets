4f16186e130c49a5c377728c63d194461b3ed8ec
ze-fernando <jfenginner935@gmail.com>
2024-03-23 14:24:12 -0300

    feat: create main.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from .main import api_route
 
 app=FastAPI()
 
@@ -9,3 +10,7 @@ app.mount(
     StaticFiles(directory='static'),
     name='static'
 )
+
+
+
+app.include_router(api_route)
--- app/main.py
+++ app/main.py
@@ -1,1 +1,32 @@
+from fastapi import (
+    APIRouter, Request, WebSocket, WebSocketDisconnect
+)
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+from .manager import ConnectionManager
+
+
+ws_manager=ConnectionManager()
+api_route=APIRouter()
+templates=Jinja2Templates(directory='templates')
+
+
+
+@api_route.get("/")
+def route(req: Request, response_classe=HTMLResponse):
+    return templates.TemplateResponse(
+        'base.html', {'request': req}
+    )
+
+
+@api_route.websocket('/{user}')
+async def push_end(ws: WebSocket, user: str):
+    await ws_manager.connect(ws)
+    try:
+        while True:
+            data=await ws.receive_text()
+            await ws_manager.broadcast(data)
+    except WebSocketDisconnect:
+        await ws_manager.disconnect(ws)
 
