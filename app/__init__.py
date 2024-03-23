cb3f8e1ea6cb7080835ad9192b4504eaaaf8bc37
ze-fernando <jfenginner935@gmail.com>
2024-03-23 12:37:09 -0300

    feat: confi __init__

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+
+app=FastAPI()
+
+app.mount(
+    '/static',
+    StaticFiles(directory='static'),
+    name='static'
+)
